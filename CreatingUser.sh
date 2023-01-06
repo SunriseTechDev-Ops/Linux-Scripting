70ed0733bbd313f9eac164d9f70449c5c41f855e
sunrise technology <noel.balingwe@yahoo.com>
2023-01-06 13:23:46 -0500

    Update CreatingUser.sh

--- CreatingUser.sh
+++ CreatingUser.sh
@@ -1,7 +1,6 @@
-#Creating a user with simple commands
-
-
+##Creating a user with simple commands
 # Creating a user
+``` sh
 echo "Please enter the user name for the account you want to create!"
 read userName
 echo "The name you entered is: "  $userName
@@ -10,3 +9,26 @@ echo "The name you entered is: "  $userName
 echo ".......User is created..........."
 echo ".......Now Set the password for  ....... $userName"
 passwd $userName
+```
+
+##Purpose - Script to add a user to Linux system including passsword
+# ------------------------------------------------------------------
+# Am i Root user?
+``` sh
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi
+```
